45aef15cd9fb30ef311acf82adaabb9ee2a4423a
黄亚男 <huangyn2@lenovo.com>
2018-08-07 11:14:52 +0800

    添加工程文件

---
+++ HYNAlbumPicker/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HYNAlbumPicker
+//
+//  Created by King on 2018/8/7.
+//  Copyright © 2018年 King. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HYNAlbumPicker/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  HYNAlbumPicker
+//
+//  Created by King on 2018/8/7.
+//  Copyright © 2018年 King. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
